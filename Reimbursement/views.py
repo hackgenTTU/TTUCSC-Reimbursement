81f8a104908de0d95a941d31acaa246aaf972965
JasonPan <jason8399@gmail.com>
2014-09-22 19:32:38 +0800

    add template

--- Reimbursement/views.py
+++ Reimbursement/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('hellow world')
+    return render_to_response('login.html')
