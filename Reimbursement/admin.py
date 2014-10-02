b7b0d0673bde6d5ceae76321653b68b9a3be52bd
JasonPan <jason8399@gmail.com>
2014-10-02 17:05:30 +0800

    delete semantic ui & update login page & update models

--- Reimbursement/admin.py
+++ Reimbursement/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from models import Cost
 # Register your models here.
+
+
+class PageCost(admin.ModelAdmin):
+    list_display = ('key', 'date', 'model', 'income', 'payout', 'invoiceNum', 'remarkNote')
+
+admin.site.register(Cost, PageCost)
--- Reimbursement/models.py
+++ Reimbursement/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cost(models.Model):
+    key = models.AutoField(primary_key=True ,unique=True)
+    date = models.DateField()
+    model = models.CharField(max_length=128)
+    income = models.IntegerField(default=0)
+    payout = models.IntegerField(default=0)
+    invoiceNum = models.CharField(max_length=20, blank=True)
+    remarkNote = models.CharField(max_length=600, blank=True)
